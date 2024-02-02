2bfa2bbe326842051467deec48f56f88fbca35c4
Mohamed Hamdi KAANICHE <124816027+MHKAANICHE@users.noreply.github.com>
2024-02-02 03:32:19 +0000

    enable testing

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+from fastapi import FastAPI
+import uvicorn
 from mylib.logic import wiki
-result = result
-print(wiki())
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return{"message" : "Wikipedia API. Call/search or /wiki"}
+
+@app.get("/search/{value}")
+async def search(value:str):
+    """Page to search in wikipedia"""
+
+    result = search_wiki(value)
+    return {"result" : result}
+
+@app.get("/wiki/{name}")
+async def wiki(name:str):
+    """Retrieve wikipedia page"""
+
+    result = wikilogic(name)
+    return {"result" : result}
+
+if __name__ == '__main__' :
+    uvicorn.run(app, port =8080, host='0.0.0.0')
